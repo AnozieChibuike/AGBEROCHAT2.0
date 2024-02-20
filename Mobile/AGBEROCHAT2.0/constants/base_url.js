d15ea1ebea8c3170a82b397e7cfd7b6e8a3b8a0a
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-02-20 03:20:44 +0100

    jk

--- Mobile/AGBEROCHAT2.0/constants/base_url.js
+++ Mobile/AGBEROCHAT2.0/constants/base_url.js
@@ -1,1 +1,2 @@
-export default base_url = 'https://flask-app-404911.uc.r.appspot.com'
+// export default base_url = 'https://flask-app-404911.uc.r.appspot.com'
+export default base_url = 'http://172.20.10.4:5000'
