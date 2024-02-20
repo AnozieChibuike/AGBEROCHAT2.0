fa4d00c73eb974131135325eb58f72a341327a20
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-02-20 16:29:39 +0100

    workflow

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo CLIENT_ID=$CLIENT_ID >> .env
+echo CLIENT_SECRET=$CLIENT_SECRET >> .env
+echo base_url=$base_url >> .env
+echo DEPLOYMENT=1 >> .env
+echo DATABASE_URL=$DATABASE_URL >> .env
+echo $CREDENTIALS >> credentials.json
+
+wc -l .env
+wc -l .credentials.json
