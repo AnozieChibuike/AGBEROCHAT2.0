0929f828bc30d8ecdcc6a3f4fb3fa473dab36215
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-03-11 08:17:35 +0100

    new push

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 echo $key >> flask.pem
 ssh -i "flask.pem" ubuntu@ec2-3-80-180-122.compute-1.amazonaws.com
 cd AGBEROCHAT2.0
-sudo systemctl stop AGBEROCHAT2_0
 git pull
+sudo systemctl stop AGBEROCHAT2_0
 sudo systemctl daemon-reload
 sudo systemctl start AGBEROCHAT2_0
 sudo systemctl enable AGBEROCHAT2_0
