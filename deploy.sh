55e5eafa85e5f2c93575d65532cc18783b206106
Ubuntu <ubuntu@ip-172-31-86-145.ec2.internal>
2024-03-11 20:11:45 +0000

    Set up and reload EC2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-echo $key >> flask.pem
-ssh -i "flask.pem" ubuntu@ec2-3-80-180-122.compute-1.amazonaws.com
-cd AGBEROCHAT2.0
-git pull
 sudo systemctl stop h
 sudo systemctl daemon-reload
 sudo systemctl start h
